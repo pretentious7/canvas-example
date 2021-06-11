d817258a3a9e27948188237860c4aded33de4891
Yang Xi Gao <joyce.gao01@gmail.com>
2021-06-11 14:59:10 +0000

    rust function returns ImageData object

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 
 // For more comments about what's going on here, check out the `hello_world`
-import('./pkg')
-  .catch(console.error);
+//import('./pkg')
+//  .catch(console.error);
+
+const wasm = import('./pkg');
+
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+let time = document.getElementById('time');
+
+wasm.then(m => {
+  let start = performance.now();
+  let imageData = m.run();
+  ctx.putImageData(imageData,0,0);
+  let end = performance.now();
+  time.textContent = "" + end-start;
+});
 
